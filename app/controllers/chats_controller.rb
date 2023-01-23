14daf20e88e288ade5ecc98da8d11017d1328c46
kakehi22222 <kakehi22222@gmail.com>
2023-01-23 11:16:38 +0900

    挨拶増えた

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -7,9 +7,15 @@ class ChatsController < ApplicationController
   end
 
   def create
-    if  params[:content].match(/%おはよう%||%こんにちは%||%こんばんは%/)
+    if  params[:content].match(/.*おはよう.*||.*おはよー.*||.*おはよ〜.*||.*おはよ!.*||おはよ/)
       Chat.create(content: params[:content])
       Chat.create(content: "おはようございます")
+    elsif params[:content].match(/.*こんにちは.*/)
+      Chat.create(content: params[:content])
+      Chat.create(content: "こんにちは")
+    elsif params[:content].match(/.*こんばんは.*/)
+      Chat.create(content: params[:content])
+      Chat.create(content: "こんばんは")
     end
     redirect_to root_path
   end
